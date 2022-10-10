949a65d4beb9b93b2967dd1ba34f7ac4950c628d
Krimsit <krimsitdjenkis@gmail.com>
2022-10-10 18:40:16 +0300

    Поправлены баги

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org',
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.openweathermap.org',
   headers: {
     'Access-Control-Allow-Origin': `*`,
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
